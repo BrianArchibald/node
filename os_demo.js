f0ed59d4580cb464d3b333ef5a090d004a70b510
Brian Archibald <brianallenarchibald@gmail.com>
2021-03-10 15:30:36 -0600

    Refactor: os functions

---
+++ os_demo.js
@@ -0,0 +1,23 @@
+const os = require('os');
+
+// Platform
+console.log(os.platform());
+
+// CPU Arch
+console.log(os.arch());
+
+// CPU Core Info
+console.log(os.cpus());
+
+// Free memory
+console.log(os.freemen());
+//
+// Total  memory
+console.log(os.totalmen());
+//
+// Home  memory
+console.log(os.homedir());
+
+//Up time
+console.log(os.uptime());
+
